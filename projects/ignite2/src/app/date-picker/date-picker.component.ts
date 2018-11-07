3c3bf69413b574aed7357e45a700ba4d95d2e429
Jeremy Hill <Jeremy_Hill@ultimatesoftware.com>
2018-11-07 09:28:30 -0500

    Added some properties to the date picker to get it to load previous values and use a dynamic name/id

--- projects/ignite2/src/app/date-picker/date-picker.component.ts
+++ projects/ignite2/src/app/date-picker/date-picker.component.ts
@@ -1,10 +1,27 @@
-import { Component } from "@angular/core";
+import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
 
 @Component({
   selector: "ignite-date-picker",
   templateUrl: "./date-picker.component.html",
   styles: []
 })
-export class IgniteDatePickerComponent {
+export class IgniteDatePickerComponent implements OnChanges {
+  startDate: Date;
+
+  @Input() id: string;
+
+  @Input() value: string;
+
+  get userId(): string {
+    return this.id + "_user";
+  }
+
   constructor() {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log("Changes have been detected: ", changes);
+    if (typeof changes["value"] !== "undefined") {
+      this.startDate = new Date(changes["value"].currentValue);
+    }
+  }
 }

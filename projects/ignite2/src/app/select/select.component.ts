ef4c2d7edcd17b546548738d9b58106d566272bd
Dmitriy Shekhovtsov <valorkin@gmail.com>
2018-12-19 22:31:28 +0200

    chore(select): alternative option for cdk-select

--- projects/ignite2/src/app/select/select.component.ts
+++ projects/ignite2/src/app/select/select.component.ts
@@ -1,4 +1,16 @@
-import {Component, EventEmitter, Input, Output, ViewEncapsulation, ContentChildren, QueryList, AfterContentInit, ViewChildren, Injectable, OnInit} from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  ViewEncapsulation,
+  ContentChildren,
+  QueryList,
+  AfterContentInit,
+  ViewChildren,
+  Injectable,
+  OnInit
+} from '@angular/core';
 import { IgniteOptionComponent } from './option.component';
 import { SelectService } from './select.service';
 
@@ -6,10 +18,22 @@ import { SelectService } from './select.service';
   selector: 'ignite-select',
   templateUrl: './select.component.html',
   styles: [
-    `:host { display: inline-block; }`,
-    `.panel-wrapper { background-color: white; border: 1px solid lightgrey; }`,
-    `.select-trigger { border: 1px lightgrey solid; }`,
-    `.fake-down-arrow { margin-left: 10px; background-color: darkgrey; color: white; padding: 0 7px; }`
+      `:host {
+      display: inline-block;
+    }`,
+      `.panel-wrapper {
+      background-color: white;
+      border: 1px solid lightgrey;
+    }`,
+      `.select-trigger {
+      border: 1px lightgrey solid;
+    }`,
+      `.fake-down-arrow {
+      margin-left: 10px;
+      background-color: darkgrey;
+      color: white;
+      padding: 0 7px;
+    }`
   ],
   encapsulation: ViewEncapsulation.ShadowDom
 })
@@ -17,19 +41,41 @@ export class IgniteSelectComponent implements OnInit {
   @Input() placeholder: string;
   @Input() value: any;
 
-  @ContentChildren(IgniteOptionComponent) options: QueryList<IgniteOptionComponent>;
+  // @ContentChildren(IgniteOptionComponent) options: QueryList<IgniteOptionComponent>;
+  private _optionComponents: IgniteOptionComponent[] = [];
+  get peepsObj(): {display: string, value: string}[] {
+    return this._optionComponents.map(item => ({display: item.display, value: item.value}));
+  }
 
   displayText = ' select value ';
 
-  constructor(private selectService: SelectService) {}
+  constructor(private selectService: SelectService) {
+  }
 
   selectId: string;
+
   ngOnInit() {
     this.selectId = 'ignite-select-' + new Date();
     this.selectService.selections.subscribe(item => {
       console.log('new option... ', JSON.stringify(item));
       this.displayText = `${item.option}`;
     });
+  }
+
+  _addMe(optionComponent: IgniteOptionComponent): void {
+    this._optionComponents.push(optionComponent);
+  }
+
+  _removeMe(optionComponent: IgniteOptionComponent): void {
+    const len = this._optionComponents.length;
+    let i = 0;
+    for (; i < len; i++) {
+      if (this._optionComponents[i] === optionComponent) {
+        this._optionComponents = [...this._optionComponents.splice(i, 1)];
+        return;
+      }
+    }
+
   }
 
   // ngAfterContentInit() {
@@ -56,6 +102,7 @@ export class IgniteSelectComponent implements OnInit {
   // }
 
   panelOpen = false;
+
   toggle() {
     this.panelOpen = !this.panelOpen;
   }
